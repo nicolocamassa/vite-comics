47f01d5247738deccdfd9e8361e47ac60f88ee0a
nicolocamassa <camassanicolo03@gmail.com>
2024-01-08 19:43:59 +0100

    [UPDATE] added header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: 'AppHeader',
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
+<template lang="">
+    <div>
+        <header>
+            <img src="../assets/dc-logo.png" alt="logo" class="logo">
+
+            <ul>
+                <li>CHARATCERS</li>
+                <li>MATTIA</li>
+                <li>CIAPE</li>
+            </ul>
+        </header>
+    </div>
+</template>
+<style lang="">
+    
+</style>
