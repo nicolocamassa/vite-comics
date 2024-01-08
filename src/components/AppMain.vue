d12e4a22c5e9cf37b2351655331850387ba8ef83
nicolocamassa <camassanicolo03@gmail.com>
2024-01-08 20:30:44 +0100

    [UPDATE] main added

---
+++ src/components/AppMain.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: 'AppMain',
+    data(){
+        return{
+            
+        }
+    }
+};
+</script>
+<template lang="">
+  <div>
+    <div class="content-row">Content goes here</div>
+  </div>
+</template>
+<style lang=""></style>
