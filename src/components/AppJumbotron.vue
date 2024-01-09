3363bbfb3dc4d0724da5f5d7eb8af66f5a2dac18
nicolocamassa <camassanicolo03@gmail.com>
2024-01-09 15:18:46 +0100

    [UPDATE] added current banner

--- src/components/AppJumbotron.vue
+++ src/components/AppJumbotron.vue
@@ -8,16 +8,28 @@ export default {
 </script>
 <template lang="">
   <div class="jumbotron">
-    <div class="current"></div>
+    <div class="current">CURRENT SERIES</div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
 .jumbotron{
   height: 400px;
   width: 100%;
   background-image: url('../assets/jumbotron.jpg');
   background-size: cover;
+  position: relative;
+
+  .current{
+    position: absolute;
+    bottom: -30px;
+    left: 250px;
+    background-color: $main_color;
+    padding: 20px 40px;
+    font-weight: 800;
+    color: white;
+  }
 }
 
 
