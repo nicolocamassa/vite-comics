bf3fc6e1427fe30b0cd20c0113db3d46c886ac4f
nicolocamassa <camassanicolo03@gmail.com>
2024-01-09 15:04:16 +0100

    [BONUS] added blue banner

--- src/components/AppBanner.vue
+++ src/components/AppBanner.vue
@@ -8,7 +8,13 @@ export default {
 </script>
 <template lang="">
   <div class="banner">
-    
+    <ul>
+        <li><img src="../assets/blue_banner/buy-comics-digital-comics.png" alt=""><span>Digital comics</span></li>
+        <li><img src="../assets/blue_banner/buy-comics-merchandise.png" alt=""><span>DC Merchandise</span></li>
+        <li><img src="../assets/blue_banner/buy-comics-subscriptions.png" alt=""><span>Subscription</span></li>
+        <li><img src="../assets/blue_banner/buy-comics-shop-locator.png" alt=""><span>comic shop locator</span></li>
+        <li><img src="../assets/blue_banner/buy-dc-power-visa.svg" alt=""><span>Dc power visa</span></li>
+    </ul>
   </div>
 </template>
 
@@ -18,6 +24,32 @@ export default {
     width: 100%;
     height: 140px;
     background-color: $main_color;
+
+    ul{
+        margin: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+        list-style: none;
+
+        li{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 0 60px;
+            color: white;
+            text-transform: uppercase;
+            
+            img{
+                width: 50px;
+                margin-right: 20px;
+            }   
+        }
+    }
 }
+
+
+
 
 </style>
